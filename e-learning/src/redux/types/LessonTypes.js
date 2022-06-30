c627ac8b2a3c0503ef5fab02e6f3dee46bf3f13b
yamisora-sys <20520930@ms.uit.edu.vn>
2022-06-30 21:25:31 +0700

    update

--- e-learning/src/redux/types/LessonTypes.js
+++
@@ -1,2 +0,0 @@
-export const SET_AUDIO = "SET_AUDIO";
-export const GET_AUDIO = "GET_AUDIO";
--- e-learning/src/utils/settings/config.js
+++
@@ -1,7 +0,0 @@
-export const USER_LOGIN = "USER_LOGIN";
-
-export const TOKEN = "access_token";
-
-export const ROLE = "role";
-
-// export const BASE_URL = "http://localhost:8080/api/v1";
