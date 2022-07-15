41012e966a9bcfcb1ff668d4d865e1a022b564b8
Tan Dung <tandung12072002@gmail.com>
2022-07-15 21:25:30 +0700

    update records

--- laravel_api/app/Http/Controllers/API/RecordController.php
+++ laravel_api/app/Http/Controllers/API/RecordController.php
@@ -13,12 +13,26 @@ class RecordController extends Controller
     public function index($id)
     {
         $lesson = Lesson::find($id);
-        // $record = Lesson::latest()->all();
-        $records = DB::table('records')->where('lesson_id', $id)->get();
+        $users = Record::where('lesson_id', $id)->groupBy('user_id')->get('user_id');
+        $data = [];
+        foreach ($users as $user) {
+            $all_record = Record::where([
+                'lesson_id' => $id,
+                'user_id' => $user->user_id,
+            ])
+                ->select('id', 'record_file', 'minute', 'second', 'created_at')
+                ->get();
+            if (count($all_record) > 0) {
+                $data[] = [
+                    'user_id' => $user->user_id,
+                    'record' => $all_record,
+                ];
+            }
+        }
         return response()->json([
             'status' => 200,
             'lesson' => $lesson,
-            'records' => $records,
+            'users' => $data,
         ]);
     }
     public function save_audio_record(Request $request)
