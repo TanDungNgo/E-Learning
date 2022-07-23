087cc0acc8b76f8b81794dd9bc1049ace781afc0
puiminh11122001 <volmicrolab@gmail.com>
2022-07-23 23:23:08 +0700

    Chuan bi admin

--- e-learning/src/utils/settings/config.js
+++ e-learning/src/utils/settings/config.js
@@ -7,4 +7,4 @@ export const ERROR = "ERROR";
 export const ROLE = "role";
 export const logoE_Learning = "/img/e-learningkaiwalogo.png";
 export const logoE_LearningVuong = "/img/e-learninglogovuong-removebg-preview.png";
-export const BASE_URL = "http://127.0.0.1:8000/api";//https://benefique-monsieur-33716.herokuapp.com/api
+export const BASE_URL = "https://benefique-monsieur-33716.herokuapp.com/api";//https://benefique-monsieur-33716.herokuapp.com/api
