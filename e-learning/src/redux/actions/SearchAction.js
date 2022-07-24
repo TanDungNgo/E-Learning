52f2e7226bb476522ebe80bf5b3b92f5e8035b01
vinh-nd2002 <nguduyvinh2k2@gmail.com>
2022-07-24 21:22:49 +0700

    fix logout

--- e-learning/src/redux/actions/SearchAction.js
+++ e-learning/src/redux/actions/SearchAction.js
@@ -1,15 +1,18 @@
-import {searchService} from '../../services/SearchService'
+import { SearchService } from "../../services/SearchService";
+import { SEARCH_COURSE } from "../types/CourseTypes";
 
 export const searchCourseAction = (searchTerm) => {
-    return async (dispatch) => {
-        try {
-            const result = await searchService.searchCourse(searchTerm);
-            dispatch({
-                type: SEARCH_COURSE,
-                value: result.courses,
-            });
-        } catch (error) {
-            console.log("error>>", error);
-        }
+  return async (dispatch) => {
+    try {
+      const result = await SearchService.searchCourse(searchTerm);
+
+      console.log("result", result);
+      dispatch({
+        type: SEARCH_COURSE,
+        value: result.courses,
+      });
+    } catch (error) {
+      console.log("error>>", error);
     }
-}
+  };
+};
