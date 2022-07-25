54c34a93ffac81d9a078c3cb95fe607390a62859
yamisora-sys <20520930@ms.uit.edu.vn>
2022-07-25 10:47:26 +0700

    update lai button

--- e-learning/src/pages/Admin/UserRequests/UpgradeRequestList.js
+++ e-learning/src/pages/Admin/UserRequests/UpgradeRequestList.js
@@ -54,7 +54,7 @@ const UpgradeRequestList = (props) => {
               <button
                 type="button"
                 className="text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:outline-none focus:ring-green-300 font-medium rounded-lg text-sm p-2.5 text-center inline-flex items-center mr-2 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800"
-                onClick={() => handleReject(item.id)}
+                onClick={() => handleAccept(item.id)}
               >
                 <svg
                   className="w-4 h-4"
@@ -72,7 +72,7 @@ const UpgradeRequestList = (props) => {
               <button
                 type="button"
                 className="text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300 font-medium rounded-lg text-sm p-2.5 text-center inline-flex items-center mr-2 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-800"
-                onClick={() => handleAccept(item.id)}
+                onClick={() => handleReject(item.id)}
               >
                 <svg
                   className="w-4 h-4"
